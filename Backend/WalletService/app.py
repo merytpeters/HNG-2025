d2e4fd5b7d6eb08ef080f52b02bf097d93c069cb
merytpeters <merytpeters@gmail.com>
2025-12-11 12:21:17 +0100

    Add HTTPBearer for authentication dependency

--- Backend/WalletService/app.py
+++ Backend/WalletService/app.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
+from fastapi.security import HTTPBearer
 from fastapi.responses import HTMLResponse
 from WalletService.user.routes import router as auth_router
 from WalletService.apikey.apikey_routes import router as apikey_router
 from WalletService.userwallet.routes import router as wallet_router
 
+
+auth_scheme = HTTPBearer()
 router = APIRouter()
 router.include_router(auth_router)
 router.include_router(apikey_router)
@@ -15,6 +18,7 @@ __all__ = ["router"]
 app = FastAPI(
     title="Wallet Service",
     description="Wallet service for Paystack deposits, transfers, and history, secured with JWT and API keys.",
+    dependencies=[Depends(auth_scheme)]
 )
 
 app.include_router(router, prefix="")
